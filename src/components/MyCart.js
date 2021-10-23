9c9b6ed7d3e0232373190b1cbc18ee5553f6e008
Jake Pu <pujianhao@126.com>
2021-10-23 17:04:55 -0500

    showing shopping cart list

--- src/components/MyCart.js
+++ src/components/MyCart.js
@@ -1,8 +1,15 @@
-import React, { useState } from 'react';
-import { Drawer, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Drawer, Button, Typography, message, List } from 'antd';
+import {getCart, checkout} from "../utils";
+
+const {Text} = Typography;
+
+
 function MyCart(props) {
     const [cartVisible, setCartVisible] = useState(false);
-
+    const [loading, setLoading] = useState(false);
+    const [cartData, setCartData] = useState({totalPrice: 0, orderItemList:[]});
+    const [checking, setChecking] = useState(false);
     const onOpenDrawer = () => {
         setCartVisible(true);
     };
@@ -10,6 +17,34 @@ function MyCart(props) {
     const onCloseDrawer = () => {
         setCartVisible(false);
     };
+    const onCheckOut = () => {
+        setChecking(true);
+        checkout()
+            .then(() => {
+                message.success("Successfully checkout");
+                setCartVisible(false);
+            })
+            .catch((err) => {
+                message.error(err.message);
+            })
+            .finally(() => {
+                setChecking(false);
+            });
+    };
+    useEffect(() => {
+        if (!cartVisible) return;
+        setLoading(true);
+        getCart()
+            .then(cartData => {
+                setCartData(cartData);
+            })
+            .catch(err => {
+                message.error(err.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    },[cartVisible]);
     return (
         <>
             <Button type="primary"
@@ -18,10 +53,48 @@ function MyCart(props) {
             >
                 Cart
             </Button>
-            <Drawer title="Basic Drawer" placement="right" onClose={onCloseDrawer} visible={cartVisible}>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
-                <p>Some contents...</p>
+            <Drawer title="My shopping Cart"
+                    placement="right"
+                    onClose={onCloseDrawer}
+                    visible={cartVisible}
+                    footer={
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}>
+                         <Text>{`Total price: ${cartData ? cartData.totalPrice.toFixed(2) : 0}`}</Text>
+                            <div>
+                                <Button>Cancel</Button>
+                                <Button
+                                    onClick={onCheckOut}
+                                    type="primary"
+                                    loading={checking}
+                                    disabled={loading || cartData?.orderItemList.length === 0}
+                                >
+                                    Checkout
+                                </Button>
+
+                            </div>
+                        </div>
+                    }
+            >
+                <List
+                    dataSource={cartData.orderItemList}
+                    loading={loading}
+                    renderItem={
+                        item => {
+                            return (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        title={item.menuItem.name}
+                                        description={`$${item.price}`}
+                                    />
+                                </List.Item>
+                            )
+                        }
+                    }
+                />
+
             </Drawer>
         </>
 
